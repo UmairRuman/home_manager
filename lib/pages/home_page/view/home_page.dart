ae59838ecfd3b35b0cc837b3bd61340831461e05
UmairRuman <programmerumair29@gmail.com>
2024-06-03 22:01:36 +0500

    NavigationBarCommit

--- lib/pages/home_page/view/home_page.dart
+++ lib/pages/home_page/view/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project_home_manager/pages/groceries_page/view/grocreis_page.dart';
 import 'package:flutter_project_home_manager/pages/home_page/widgets/bottom_navigation_bar.dart';
 import 'package:flutter_project_home_manager/pages/overview_page/view/overview_page.dart';
+import 'package:flutter_project_home_manager/pages/overview_page/widgets/bills_expenses_text.dart';
 import 'package:flutter_project_home_manager/pages/profile_page/view/profile_page.dart';
 import 'package:flutter_project_home_manager/pages/utilities_page/view/utilities_page.dart';
 import 'package:flutter_project_home_manager/pages/weather_page/view/weather_page.dart';
@@ -16,52 +17,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //page controller
-  late final PageController _pageController;
+  int currentIndex = 0;
+  //pages
+  static const _firstPageIndex = 0;
+  static const _secondPageIndex = 1;
+  static const _thirdPageIndex = 2;
+  static const _fourthPageIndex = 3;
+  static const _fifthPageIndex = 4;
 
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: 0);
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
+  void changeState() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    var Size(:width) = MediaQuery.sizeOf(context);
-    return PopScope(
-      canPop: false,
-      child: Center(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          fit: StackFit.loose,
-          children: [
-            PageView(
-              allowImplicitScrolling: false,
-              physics: const NeverScrollableScrollPhysics(),
-              controller: _pageController,
-              scrollDirection: Axis.horizontal,
-              children: const [
-                OverviewPage(),
-                UtilityPage(),
-                WeatherPage(),
-                GroceryPage(),
-                ProfilePage(),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.02),
-              child: HomePageBottomNavigationBar(
-                pageController: _pageController,
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      body: StatefulBuilder(
+        builder: (context, setState) {
+          if (currentIndex == _firstPageIndex) {
+            return const OverviewPage();
+          } else if (currentIndex == _secondPageIndex) {
+            return const GroceryPage();
+          } else if (currentIndex == _thirdPageIndex) {
+            return const WeatherPage();
+          } else if (currentIndex == _fourthPageIndex) {
+            return const UtilityPage();
+          } else {
+            return const ProfilePage();
+          }
+        },
+      ),
+      bottomNavigationBar: HomePageBottomNavigationBar(
+        changeState: changeState,
+        currentIndex: currentIndex,
       ),
     );
   }
--- lib/pages/home_page/widgets/bottom_navigation_bar.dart
+++ lib/pages/home_page/widgets/bottom_navigation_bar.dart
@@ -1,62 +1,101 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePageBottomNavigationBar extends StatelessWidget {
-  const HomePageBottomNavigationBar({super.key, required this.pageController});
-
-  final PageController pageController;
-
-  //pages
-  static const _firstPageIndex = 0;
-  static const _secondPageIndex = 1;
-  static const _thirdPageIndex = 2;
-  static const _fourthPageIndex = 3;
-  static const _fifthPageIndex = 4;
+  HomePageBottomNavigationBar(
+      {super.key, required this.currentIndex, required this.changeState});
+  int currentIndex;
+  VoidCallback changeState;
+  final NotchBottomBarController notchBottomBarController =
+      NotchBottomBarController(index: 0);
 
   // icon labels
 
   @override
   Widget build(BuildContext context) {
-    final Size(:height) = MediaQuery.sizeOf(context);
-    int currentIndex = 0;
-
-    void _onBottomIconTap(int index) {
-      //switch to current page based on index
-      switch (index) {
-        case 0:
-          pageController.jumpToPage(
-            _firstPageIndex,
-          );
-          break;
-        case 1:
-          pageController.jumpToPage(_secondPageIndex);
-          break;
-        case 2:
-          pageController.jumpToPage(_thirdPageIndex);
-          break;
-        case 3:
-          pageController.jumpToPage(_fourthPageIndex);
-          break;
-        default:
-          pageController.jumpToPage(_fifthPageIndex);
-      }
-      //assign current index variable the index coming from parameter
-      currentIndex = index;
-    }
-
-    final selectedTextStyle = TextStyle(
-        color: Colors.black,
-        fontSize: height * 0.015,
-        fontWeight: FontWeight.bold);
-
-    return StatefulBuilder(
-      builder: (context, setState) {
-        return BottomNavigationBar(items: [
-          BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'First'),
-          BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Second'),
-        ]);
-
-        /// items need to be added
+    return AnimatedNotchBottomBar(
+      removeMargins: true,
+      notchGradient: const LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Colors.red,
+          Colors.green,
+        ],
+      ),
+      notchColor: Colors.white,
+      color: Colors.amber,
+      bottomBarWidth: MediaQuery.of(context).size.width,
+      bottomBarHeight: 80,
+      notchBottomBarController: notchBottomBarController,
+      onTap: (int value) {
+        currentIndex = value;
+        changeState();
       },
+      bottomBarItems: const [
+        //First Item
+        BottomBarItem(
+          inActiveItem: Icon(
+            Icons.document_scanner_outlined,
+            color: Colors.blueGrey,
+          ),
+          activeItem: Icon(
+            Icons.document_scanner,
+            color: Colors.blueAccent,
+          ),
+          itemLabel: 'Overview',
+        ),
+        //Second Item
+        BottomBarItem(
+          inActiveItem: Icon(
+            Icons.shopping_cart_outlined,
+            color: Colors.blueGrey,
+          ),
+          activeItem: Icon(
+            Icons.shopping_cart,
+            color: Colors.blueAccent,
+          ),
+          itemLabel: 'Groceries',
+        ),
+        //ThirdItem
+        BottomBarItem(
+          inActiveItem: Icon(
+            Icons.cloud_outlined,
+            color: Colors.blueGrey,
+          ),
+          activeItem: Icon(
+            Icons.cloud,
+            color: Colors.blueAccent,
+          ),
+          itemLabel: 'Weather',
+        ),
+        //Fourth Item
+        BottomBarItem(
+          inActiveItem: Icon(
+            Icons.edit_document,
+            color: Colors.blueGrey,
+          ),
+          activeItem: Icon(
+            Icons.edit_document,
+            color: Colors.blueAccent,
+          ),
+          itemLabel: 'Bills',
+        ),
+        //Fifth Item
+        BottomBarItem(
+          inActiveItem: Icon(
+            Icons.person_2_outlined,
+            color: Colors.blueGrey,
+          ),
+          activeItem: Icon(
+            Icons.person_2_outlined,
+            color: Colors.blueAccent,
+          ),
+          itemLabel: 'Profile',
+        ),
+      ],
+      kBottomRadius: 0,
+      kIconSize: 20,
     );
   }
 }
